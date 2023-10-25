71da337b60be499386fdfcaf20d128f6bbbe840e
puHoraira <mdabu-2021911202@cs.du.ac.bd>
2023-10-25 23:42:57 +0600

    co

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # project1-2
+# updated
+# updated
--- tictactoemain.c
+++ tictactoemain.c
@@ -101,7 +101,7 @@ int main(int argc, char *argv[])
     }
     else if (selector == chess)
     {
-    }
+        }
     else if (selector == credit)
     {
         creditpagesec();
